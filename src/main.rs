e2182c70d3b11e9f559ae0457abe83c86df22a98
фыв <dianadoenko5@gmail.com>
2023-06-07 23:38:59 +0300

    main.rs update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::fs;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
 use pest::Parser;
 
 use hasan::{tokenizer, parser};
@@ -7,19 +10,35 @@ use parser::ASTParser;
 
 const FILE_PATH: &str = "./input.hsl";
 
-fn safe_file_read(path: &str) -> String {
-    fs::read_to_string(path).unwrap_or("".to_owned())
+fn read_file(path: &str) -> String {
+    let file = File::open(path).expect(&format!("Failed to open file \"{}\" (read)", path));
+
+    let mut reader = BufReader::new(file);
+    let mut contents = String::new();
+
+    reader.read_to_string(&mut contents).expect(&format!("Failed to read from file \"{}\"", path));
+    contents
+}
+
+fn write_file(path: &str, contents: String) {
+    let mut file = File::create(path).expect(&format!("Failed to open file \"{}\" (write)", path));
+    file.write_all(contents.as_bytes()).expect(&format!("Failed to write to file \"{}\"", path));
 }
 
 fn main() {
-    let contents = safe_file_read(FILE_PATH);
+    println!("Pest parsing...");
+
+    let contents = read_file(FILE_PATH);
     let pairs = HasanPestParser::parse(Rule::program, &contents).expect("Failed to parse input");
 
-    println!("parsed tokens: {}", pairs);
-    println!();
+    write_file("./compiled/1_raw_ast.txt", format!("{:#?}", pairs));
+
+    println!("AST parsing...");
 
     let ast_parser = ASTParser::new(pairs);
     let ast = ast_parser.parse();
 
-    println!("parsed ast ({}): {:#?}", ast.len(), ast);
+    write_file("./compiled/2_hasan_ast.txt", format!("{:#?}", ast));
+
+    println!("Done!");
 }
