586059c4d9b8a759458dc59e652582369d46b704
greenbush5 <dianadoenko5@gmail.com>
2023-07-16 19:14:15 +0300

    added a cli

---
+++ src/cli.rs
@@ -0,0 +1,71 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+pub struct CLI {
+	#[clap(subcommand)]
+	/// Action
+	pub subcommand: CLISubcommand
+}
+
+impl CLI {
+	pub fn parse_custom() -> Self {
+		CLI::parse()
+	}
+}
+
+#[derive(Debug, Subcommand)]
+pub enum CLISubcommand {
+	/// Compile a file
+	Compile(CompileCommand),
+
+	/// Create a new test case
+	Test(TestSubcommand)
+}
+
+#[derive(Debug, Args)]
+pub struct CompileCommand {
+	#[arg(short, long, default_value = "./input.hsl")]
+	/// Path of the target file
+	pub file_path: String,
+
+	#[arg(short, long, default_value_t = false)]
+	/// Show debug information
+	pub debug: bool
+}
+
+#[derive(Debug, Args)]
+pub struct TestSubcommand {
+	#[clap(subcommand)]
+	pub command: TestCommand
+}
+
+#[derive(Debug, Subcommand)]
+pub enum TestCommand {
+	/// Create a test case
+	Create(CreateTestCommand),
+
+	/// Update a test case
+	Update(UpdateTestCommand),
+
+	/// Delete a test case
+	Delete(DeleteTestCommand)
+}
+
+#[derive(Debug, Args)]
+pub struct CreateTestCommand {
+	/// Name of the test file to create
+	pub name: String
+}
+
+#[derive(Debug, Args)]
+pub struct UpdateTestCommand {
+	/// Name of the test file to update
+	pub name: String
+}
+
+#[derive(Debug, Args)]
+pub struct DeleteTestCommand {
+	/// Name of the test file to delete
+	pub name: String
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod cli;
 pub mod tokenizer;
 pub mod parser;
 pub mod compiler;
