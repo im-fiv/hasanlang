06c3877a50dc216548272fcc71e4f932bea9af70
greenbush5 <dianadoenko5@gmail.com>
2023-07-25 23:50:30 +0300

    scratched semantic analysis

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,2 +1,18 @@
-pub mod analyzer;
-pub mod types;
+use anyhow::Error;
+use crate::hasan_parser::Program;
+
+pub struct SemanticAnalyzer {
+	#[allow(dead_code)]
+	ast: Program
+}
+
+impl SemanticAnalyzer {
+	pub fn new(ast: Program) -> Self {
+		SemanticAnalyzer { ast }
+	}
+
+	pub fn analyze(&self) -> Result<(), Error> {
+		// TODO: Implement semantic analysis when I have a better understanding of Rust
+		Ok(())
+	}
+}
