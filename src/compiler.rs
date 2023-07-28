ce1af7e006646775a9b803c1e1dcf8a2c948ccff
greenbush5 <dianadoenko5@gmail.com>
2023-07-29 00:02:44 +0300

    compiler update

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,9 +1,10 @@
+use inkwell::AddressSpace;
 use inkwell::context::Context;
 use inkwell::builder::Builder;
 use inkwell::passes::PassManager;
 use inkwell::module::Module;
 use inkwell::types::{BasicMetadataTypeEnum, BasicTypeEnum};
-use inkwell::values::{FunctionValue, PointerValue, IntValue, FloatValue, BasicValue};
+use inkwell::values::{FunctionValue, PointerValue, IntValue, FloatValue, BasicValue, BasicValueEnum, GlobalValue, InstructionValue};
 
 use std::collections::HashMap;
 use strum_macros::Display;
@@ -12,6 +13,20 @@ use anyhow::{Error, bail};
 use crate::hasan_parser::{self as P};
 
 const ENTRY_FUNCTION_NAME: &'static str = "main";
+
+/// Generates a random string given its length
+pub fn random_string(length: usize) -> String {
+	// I wonder, is there a real possibility that it will generate the same string at some point?
+
+	use rand::{thread_rng, Rng};
+	use rand::distributions::Alphanumeric;
+
+	thread_rng()
+		.sample_iter(&Alphanumeric)
+		.take(length)
+		.map(char::from)
+		.collect()
+}
 
 #[derive(Debug, Clone)]
 pub struct FunctionPrototype {
@@ -58,12 +73,30 @@ pub struct Compiler<'a, 'ctx> {
 pub enum ExpressionValue<'ctx> {
 	Int(IntValue<'ctx>),
 	Float(FloatValue<'ctx>),
-	String(FloatValue<'ctx>),
+	String(GlobalValue<'ctx>),
 	Boolean(IntValue<'ctx>)
 }
 
 impl<'ctx> ExpressionValue<'ctx> {
-	pub fn compile_unary_minus(&self) -> Result<Self, Error> {
+	/// Unwraps inner LLVM value and converts it into `BasicValueEnum`
+	pub fn into_basic_value_enum(&self) -> BasicValueEnum<'ctx> {
+		/// Allows to get rid of repetition while having to write the same piece of code for every match arm
+		macro_rules! v {
+			($value:ident) => {
+				$value.as_basic_value_enum()
+			}
+		}
+
+		match self {
+			Self::Int(value) => v!(value),
+			Self::Float(value) => v!(value),
+			Self::String(value) => v!(value),
+			Self::Boolean(value) => v!(value),
+		}
+	}
+
+	/// Applies unary `-` to the inner LLVM value. Errors if the type is not compatible with the operation
+	pub fn apply_unary_minus(&self) -> Result<Self, Error> {
 		match self {
 			Self::Int(value) => Ok(Self::Int(value.const_neg())),
 			Self::Float(value) => Ok(Self::Float(value.const_neg())),
@@ -72,10 +105,54 @@ impl<'ctx> ExpressionValue<'ctx> {
 		}
 	}
 
-	pub fn compile_unary_not(&self) -> Result<Self, Error> {
+	/// Applies unary `not` to the inner LLVM value. Errors if the type is not compatible with the operation
+	pub fn apply_unary_not(&self) -> Result<Self, Error> {
 		match self {
 			Self::Boolean(value) => Ok(Self::Boolean(value.const_not())),
 			_ => bail!("Cannot perform unary operation `{}` on a value of type `{}`", "not", self.to_string())
+		}
+	}
+}
+
+#[derive(Debug, Clone, Display)]
+pub enum BuiltinType {
+	Int,
+	Float,
+	String,
+	Boolean
+}
+
+impl<'ctx> BuiltinType {
+	pub fn as_llvm_type(&self, context: &'ctx Context) -> BasicMetadataTypeEnum<'ctx> {
+		match self {
+			Self::Int => BasicMetadataTypeEnum::IntType(context.i64_type()),
+			Self::Float => BasicMetadataTypeEnum::FloatType(context.f64_type()),
+			Self::String => BasicMetadataTypeEnum::PointerType(context.i8_type().ptr_type(AddressSpace::default())),
+			Self::Boolean => BasicMetadataTypeEnum::IntType(context.bool_type())
+		}
+	}
+
+	pub fn as_hasan_str(&self) -> &'static str {
+		match self {
+			Self::Int => "int",
+			Self::Float => "float",
+			Self::String => "string",
+			Self::Boolean => "bool"
+		}
+	}
+}
+
+impl TryFrom<&str> for BuiltinType {
+	type Error = Error;
+
+	fn try_from(value: &str) -> Result<Self, Self::Error> {
+		match value {
+			"int" => Ok(Self::Int),
+			"float" => Ok(Self::Float),
+			"string" => Ok(Self::String),
+			"bool" => Ok(Self::Boolean),
+
+			_ => bail!("Failed to convert `{}` into a built-in type", value)
 		}
 	}
 }
@@ -97,17 +174,21 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
 		}
 	}
 
-	pub fn compile(&mut self, program: &P::Program) -> Result<FunctionValue<'ctx>, Error> {
+	pub fn compile(&mut self, program: &P::Program) -> Result<(), Error> {
 		self.compile_statements(&program.statements)?;
 
-		self.current_function.ok_or(Error::msg("No current function is present"))
+		if self.get_function(ENTRY_FUNCTION_NAME).is_none() {
+			bail!("No entry function definition `{}` has been found", ENTRY_FUNCTION_NAME);
+		}
+
+		Ok(())
 	}
 
 	fn compile_statements(&mut self, statements: &Vec<P::Statement>) -> Result<(), Error> {
 		for statement in statements {
 			match statement {
 				P::Statement::FunctionDefinition(function) => { self.compile_function(function.to_owned())?; },
-				P::Statement::Return(value) => { self.compile_return(value.to_owned()); },
+				P::Statement::Return(value) => { self.compile_return(value.to_owned())?; },
 
 				_ => panic!("Encountered unknown statement `{}`", statement.to_string())
 			}
@@ -145,9 +226,9 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
 		builder.build_alloca(self.context.f64_type(), name)
 	}
 
-	fn int_value(&self, number: P::IntType) -> IntValue<'ctx> {
-		let is_negative = number < 0;
-		let converted = number.abs() as u64;
+	fn int_value(&self, value: P::IntType) -> IntValue<'ctx> {
+		let is_negative = value < 0;
+		let converted = value.abs() as u64;
 
 		let value = self.context.i64_type().const_int(converted, false);
 
@@ -159,13 +240,43 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
 	}
 
 	#[inline]
-	fn float_value(&self, number: P::FloatType) -> FloatValue<'ctx> {
-		self.context.f64_type().const_float(number)
+	fn float_value(&self, value: P::FloatType) -> FloatValue<'ctx> {
+		self.context.f64_type().const_float(value)
 	}
 
-	pub fn compile_type(&self, _kind: &P::Type) -> Result<BasicMetadataTypeEnum<'ctx>, Error> {
-		// TODO: Properly implement this function
-		Ok(BasicMetadataTypeEnum::IntType(self.context.i64_type()))
+	#[inline]
+	fn string_value(&self, value: String) -> GlobalValue<'ctx> {
+		unsafe {
+			self.builder.build_global_string(&value, &format!("str_{}", random_string(30)))
+		}
+	}
+
+	#[inline]
+	fn bool_value(&self, value: bool) -> IntValue<'ctx> {
+		self.context.bool_type().const_int(value as u64, false)
+	}
+
+	pub fn compile_type(&self, kind: &P::Type) -> Result<BasicMetadataTypeEnum<'ctx>, Error> {
+		match kind {
+			P::Type::Regular(regular_type) => {
+				let P::RegularType { base, generics: _, raw: _, array } = regular_type;
+
+				if *array {
+					// TODO: Implement this
+					unimplemented!("Array types are currently not supported");
+				}
+
+				let resolved_type = BuiltinType::try_from(base.as_str())?
+					.as_llvm_type(self.context);
+
+				Ok(resolved_type)
+			},
+
+			P::Type::Function(_function_type) => {
+				// TODO: Implement function types
+				unimplemented!("Function types are currently not supported")
+			}
+		}
 	}
 
 	/// Compiles the provided function prototype
@@ -183,26 +294,40 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
 		
 		let argument_types = argument_types.as_slice();
 
+		macro_rules! f {
+			($t:ident) => {
+				$t.fn_type(argument_types, false)
+			};
+		}
+
+		use BasicMetadataTypeEnum as BMTE;
+
 		let fn_type = match return_type {
-			BasicMetadataTypeEnum::ArrayType(t) => t.fn_type(argument_types, false),
-			BasicMetadataTypeEnum::FloatType(t) => t.fn_type(argument_types, false),
-			BasicMetadataTypeEnum::IntType(t) => t.fn_type(argument_types, false),
-			BasicMetadataTypeEnum::PointerType(t) => t.fn_type(argument_types, false),
-			BasicMetadataTypeEnum::StructType(t) => t.fn_type(argument_types, false),
-			BasicMetadataTypeEnum::VectorType(t) => t.fn_type(argument_types, false),
-			BasicMetadataTypeEnum::MetadataType(t) => t.fn_type(argument_types, false)
+			BMTE::ArrayType(t) => f!(t),
+			BMTE::FloatType(t) => f!(t),
+			BMTE::IntType(t) => f!(t),
+			BMTE::PointerType(t) => f!(t),
+			BMTE::StructType(t) => f!(t),
+			BMTE::VectorType(t) => f!(t),
+			BMTE::MetadataType(t) => f!(t)
 		};
 
 		let fn_value = self.module.add_function(&prototype.name, fn_type, None);
 
 		for (index, argument) in fn_value.get_param_iter().enumerate() {
+			macro_rules! f {
+				($argument:expr) => {
+					$argument.set_name(&prototype.arguments[index].name)
+				};
+			}
+
 			match argument.get_type() {
-				BasicTypeEnum::ArrayType(_) => argument.into_array_value().set_name(&prototype.arguments[index].name),
-				BasicTypeEnum::FloatType(_) => argument.into_float_value().set_name(&prototype.arguments[index].name),
-				BasicTypeEnum::IntType(_) => argument.into_int_value().set_name(&prototype.arguments[index].name),
-				BasicTypeEnum::PointerType(_) => argument.into_pointer_value().set_name(&prototype.arguments[index].name),
-				BasicTypeEnum::StructType(_) => argument.into_struct_value().set_name(&prototype.arguments[index].name),
-				BasicTypeEnum::VectorType(_) => argument.into_vector_value().set_name(&prototype.arguments[index].name),
+				BasicTypeEnum::ArrayType(_) => f!(argument.into_array_value()),
+				BasicTypeEnum::FloatType(_) => f!(argument.into_float_value()),
+				BasicTypeEnum::IntType(_) => f!(argument.into_int_value()),
+				BasicTypeEnum::PointerType(_) => f!(argument.into_pointer_value()),
+				BasicTypeEnum::StructType(_) => f!(argument.into_struct_value()),
+				BasicTypeEnum::VectorType(_) => f!(argument.into_vector_value())
 			}
 		}
 
@@ -256,19 +381,11 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
 		}
 	}
 
-	fn compile_return(&mut self, value: P::Expression) -> Result<(), Error> {
-		// TODO: Properly implement this function
+	fn compile_return(&mut self, value: P::Expression) -> Result<InstructionValue, Error> {
+		let compiled = self.compile_expression(&value)?;
+		let basic_value = compiled.into_basic_value_enum();
 
-		if let P::Expression::Int(expression_value) = value {
-			let as_int = self.int_value(expression_value);
-			let as_basic = as_int.as_basic_value_enum();
-	
-			self.builder.build_return(Some(&as_basic));
-	
-			Ok(())
-		} else {
-			unreachable!()
-		}
+		Ok(self.builder.build_return(Some(&basic_value)))
 	}
 
 	/// Compiles the provided `Expression` into LLVM number value (`FloatValue` or `IntValue`)
@@ -278,8 +395,8 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
 		match expression.to_owned() {
 			E::Int(value) => Ok(ExpressionValue::Int(self.int_value(value))),
 			E::Float(value) => Ok(ExpressionValue::Float(self.float_value(value))),
-			E::String(value) => Ok(ExpressionValue::Int(self.int_value(1337))), // TODO: fix this
-			E::Boolean(value) => Ok(ExpressionValue::Boolean(self.int_value(value as P::IntType))),
+			E::String(value) => Ok(ExpressionValue::String(self.string_value(value))),
+			E::Boolean(value) => Ok(ExpressionValue::Boolean(self.bool_value(value))),
 
 			E::Unary { operator, operand } => self.compile_unary_expression(operator, operand),
 
@@ -292,8 +409,8 @@ impl<'a, 'ctx> Compiler<'a, 'ctx> {
 		let compiled_expression = self.compile_expression(operand.as_ref())?;
 
 		match operator {
-			P::UnaryOperator::Minus => compiled_expression.compile_unary_minus(),
-			P::UnaryOperator::Not => compiled_expression.compile_unary_not()
+			P::UnaryOperator::Minus => compiled_expression.apply_unary_minus(),
+			P::UnaryOperator::Not => compiled_expression.apply_unary_not()
 		}
 	}
 }
