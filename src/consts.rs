c57bdbb6bcc8b2b97ae39dbc61b3574e7643aa29
greenbush5 <dianadoenko5@gmail.com>
2023-08-20 17:04:01 +0300

    renamed folder

--- src/consts.rs
+++ src/consts.rs
@@ -2,14 +2,14 @@ use const_format::formatcp;
 
 type CStr = &'static str;
 
-/// Formats the input string into `./compiled/{string}`
+/// Formats the input string into `./build/{string}`
 #[inline]
 pub fn fmt_c(input: CStr) -> String {
 	format!("{}/{}", OUT_DIR_PATH, input)
 }
 
 pub const DEFAULT_INPUT_FP: CStr = "./input.hsl";
-pub const OUT_DIR_PATH: CStr = "./compiled";
+pub const OUT_DIR_PATH: CStr = "./build";
 
 pub const SOURCE_FN: CStr = "1_source.hsl";
 pub const PEST_AST_FN: CStr = "2_pest_ast.txt";
